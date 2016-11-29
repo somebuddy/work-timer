0a2b9d2932cfce02ed2a3bd997f39d0cec520e6d
Somebuddy <oleksiy.drogovozov@gmail.com>
2016-11-29 16:20:35 +0200

    Add activity base actions stubs

--- client/app/time-tracker/activities.component.ts
+++ client/app/time-tracker/activities.component.ts
@@ -9,17 +9,84 @@ const ACTIVITIES: Timer[] = [
 @Component({
   selector: 'box-activities',
   template: `
+    <!-- current activity -->
+    <div class="activity" *ngIf="currentActivity">
+      <span>Now working on:</span>
+      <span class="title">{{ currentActivity.activity }}</span>
+      <span class="time">{{ currentActivity.currentTime }}</span>
+      <button class="btn btn-stop" (click)="onStop(currentActivity)">Stop</button>
+      <button class="btn btn-done" (click)="onDone(currentActivity)">Done</button>
+    </div>
+
+    <!-- all activities -->
     <div class="activities">
       <header>Previous Activities</header>
       <ul class="content">
-        <li *ngFor="let act of activities">
-          <div class="title">{{ act.activity }}</div>
-          <div class="time">{{ act.currentTime }}</div>
+        <li *ngFor="let act of activities"
+          class="activity"
+          [class.selected]="act === selectedActivity"
+          [class.done]="act.done"
+          (click)="onSelect(act)">
+          <span class="title">{{ act.activity }}</span>
+          <span class="time">{{ act.currentTime }}</span>
+          <button class="btn btn-start"
+            *ngIf="!act.done"
+            (click)="onStart(act)">Start</button>
+          <button class="btn btn-done" (click)="onDone(act)">Done</button>
         </li>
       </ul>
     </div>
-  `
+  `,
+  styles: [`
+    .activity {
+      padding: 1rem 2rem;
+      margin: 1rem 0;
+    }
+
+    .activity > .title {
+      display: inline-block;
+      margin-right: auto;
+    }
+
+    .activity > .time {
+      display: inline-block;
+      margin-right: 1rem;
+      font-weight: 600;
+    }
+
+    .activity.selected {
+      color: orangered;
+      font-weight: 600;
+    }
+
+    .activity.done > .title {
+      text-decoration: line-through;
+    }
+  `]
 })
 export class ActivitiesListComponent {
   activities = ACTIVITIES;
+  currentActivity: Timer;
+  selectedActivity: Timer;
+
+  onSelect(activity: Timer):void {
+    this.selectedActivity = activity;
+  };
+
+  onStart(activity: Timer): void {
+    this.currentActivity = activity;
+    activity.startedAt = new Date();
+  };
+
+  onStop(activity: Timer): void {
+    activity.stoppedAt = new Date();
+    activity.currentTime = (activity.currentTime || 0) + (activity.stoppedAt - activity.startedAt);
+    this.currentActivity = undefined;
+  };
+
+  onDone(activity: Timer): void {
+    activity.done = true;
+    activity.doneAt = new Date();
+    console.log('Finished: ', activity);
+  };
 };
